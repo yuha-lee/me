d8284f3f38f803aeccfa9d4d20b0f8e2a880def4
Yuha Lee <yuha.lee.dev@gmail.com>
2021-08-24 20:21:58 +0900

    Fix index path

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
     mode: "development",
-    entry: "./src/Index.tsx",
+    entry: "./Index.tsx",
     output: {
         filename: "index.js",
         path: path.join(__dirname, "/dist"),
