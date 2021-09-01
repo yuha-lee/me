de88973080a368ad1068c2274d509c6ddebb3378
Yuha Lee <yuha.lee.dev@gmail.com>
2021-09-02 00:10:42 +0900

    delete import lines

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -2,8 +2,6 @@ import { ThemeProvider, CssBaseline, Container, Box } from '@material-ui/core';
 import { ReactNode } from 'react';
 import defaultTheme from '../defaultTheme';
 import GNB from './GNB';
-import Footer from './Footer';
-import FloatingMenu from './FloatingMenu';
 
 type PageProps = {
     content: ReactNode;
